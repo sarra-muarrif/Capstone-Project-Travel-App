5851d95a43066483c7c063de129a09a0b93bff76
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-02-25 19:25:28 +0200

    fix client server

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,93 +1,107 @@
 import fetch from "node-fetch";
 import { getCityInfo, getWetherInfo, getCityImg } from "./requests";
 
-export const handleSearch = async (e) => {
-    e.preventDefault()
-    const destinationCity = document.getElementById("destination").value;
-    const departingTime = document.getElementById("departing-time").value;
-    const returingTime = document.getElementById("returing").value;
-    const current_time = new Date().getTime()
-    const dept_time = new Date(departingTime).getTime()
-    const return_time = new Date(returingTime).getTime()
-    const daysLeft = Math.ceil((dept_time - current_time) / (1000 * 60 * 60 * 24));
-    const duration = Math.ceil((return_time - dept_time) / (1000 * 60 * 60 * 24));
-
-    //Call getCityIngo Function to get Data of City 
-    getCityInfo(destinationCity)
-        .then((cityData) => {
-            const { lng, lat } = cityData.geonames[0]
-            //Call getWetherInfo Function to get Data of Weather  
-            const weatherData = getWetherInfo(lat, lng);
-            return weatherData
-        }).then((weatherData) => {
-            const { city_name, country_code } = weatherData
-            const { temp } = weatherData.data[0]
-            const { description } = weatherData.data[0].weather
-            //Call postData Function to Post Data to local server  
-            const userData = postData('http://localhost:5000/trip', {
-                departingTime,
-                returingTime,
-                city_name,
-                country_code,
-                temp,
-                description,
-                duration
-            });
-            return userData;
-        }).then((userData) => {
-            //Call updateUI Function to Display User Trip   
-            updateUI(userData)
-        })
+function getDuration(date1, date2) {
+  const diffTime = Math.abs(new Date(date2) - new Date(date1));
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
+
+export const handleSearch = async () => {
+  // Get user values
+  const destinationCity = document.getElementById("destination").value;
+  const departingTime = document.getElementById("departing-time").value;
+  const returningTime = document.getElementById("returning-time").value;
+  // Calculate The Duration
+  const duration = getDuration(departingTime, returningTime);
+  // Get Information for target city
+  getCityInfo(destinationCity)
+    .then((cityData) => {
+      const { lng, lat } = cityData.geonames[0];
+      //Call getWetherInfo Function to get Data of Weather
+      return getWetherInfo(lat, lng);
+    })
+    .then((weatherData) => {
+      const { city_name, country_code } = weatherData;
+      const { temp } = weatherData.data[0];
+      const { description } = weatherData.data[0].weather;
+      //Call postData Function to Post Data to local server
+      const userData = postData("http://localhost:5000/trip", {
+        departingTime,
+        returningTime,
+        city_name,
+        country_code,
+        temp,
+        description,
+        duration,
+      });
+      return userData;
+    })
+    .then((userData) => {
+      //Call updateUI Function to Display User Trip
+      updateUI(userData);
+    });
+};
 
 // Function postData to POST data to our local server
-const postData = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            departingTime: data.departingTime,
-            returingTime: data.returingTime,
-            city_name: data.city_name,
-            country_code: data.country_code,
-            temp: data.temp,
-            description: data.description,
-            duration: data.duration
-
-        })
-    });
-    try {
-        const newData = await response.json();
-        return newData;
-
-    } catch (error) {
-        console.log(error, "error")
-    }
-}
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      departingTime: data.departingTime,
+      returningTime: data.returningTime,
+      city_name: data.city_name,
+      country_code: data.country_code,
+      temp: data.temp,
+      description: data.description,
+      duration: data.duration,
+    }),
+  });
+  try {
+    return await response.json();
+  } catch (error) {
+    console.log(error, "error");
+  }
+};
 
 // Function update UI that reveals the results page with updated trip information including fetched image of the destination
 const updateUI = async (userData) => {
-    const { departingTime, returingTime, city_name, temp, country_code, description, duration } = userData
-    const img = await getCityImg(city_name)
-    console.log(img, "image")
-    const { largeImageURL } = img.hits[0];
-    document.querySelector(".section-result").style.display = "block"
-    document.getElementById("city_image").setAttribute("src", `${largeImageURL}`);
-    document.getElementById("city_name").innerHTML = ` You are going to:${city_name}`;
-    document.getElementById("country_code").innerHTML = `${country_code}`;
-    document.getElementById("departingTime").innerHTML = `Departing on :${departingTime}`;
-    document.getElementById("returingTime").innerHTML = `Returing on :${returingTime}`;
-    document.getElementById("temp").innerHTML = `Expect weather to be :${temp}`;
-    document.getElementById("description").innerHTML = `${description}`;
-    document.getElementById("durationTime").innerHTML = `This is ${duration} days`;
-}
+  const {
+    departingTime,
+    returningTime,
+    city_name,
+    temp,
+    country_code,
+    description,
+    duration,
+  } = userData;
+  const img = await getCityImg(city_name);
+  const { largeImageURL } = img.hits[0];
+  document.querySelector(".section-result").style.display = "block";
+  document.getElementById("city_image").setAttribute("src", `${largeImageURL}`);
+  document.getElementById(
+    "city_name"
+  ).innerHTML = ` You are going to:${city_name}`;
+  document.getElementById("country_code").innerHTML = `${country_code}`;
+  document.getElementById(
+    "departingTime"
+  ).innerHTML = `Departing on :${departingTime}`;
+  document.getElementById(
+    "returningTime"
+  ).innerHTML = `Returning on :${returningTime}`;
+  document.getElementById("temp").innerHTML = `Expect weather to be :${temp}`;
+  document.getElementById("description").innerHTML = `${description}`;
+  document.getElementById(
+    "durationTime"
+  ).innerHTML = `This is ${duration} days`;
+};
 
-
-
-//Event listener to add function to exisiting HTML DOM element
-document.querySelector(".search-btn").addEventListener("click", handleSearch)
-
+//Event listener to add function to existing HTML DOM element
+document.querySelector(".search-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  handleSearch();
+});
--- src/client/js/requests.js
+++ src/client/js/requests.js
@@ -5,50 +5,44 @@ const GEONAMES_URL = "http://api.geonames.org/searchJSON";
 const GEONAMES_API_KEY = "sarra";
 const WEATHERBIT_FORCAST_URL = "https://api.weatherbit.io/v2.0/forecast/daily";
 const WEATHERBIT_API_KEY = "e77f3a905d2a4f55ae18d2168074848e";
-const PIXALABAY_URL = "https://pixabay.com/api/"
-const PIXALABAY_API_KEY = "20310833-4d8070e75a1507d44bb697092"
+const PIXALABAY_URL = "https://pixabay.com/api/";
+const PIXALABAY_API_KEY = "20310833-4d8070e75a1507d44bb697092";
 
-//Function getCityInfo to get city information from Geonames API (latitude, longitude)
+// Function getCityInfo to get city information from Geonames API (latitude, longitude)
 const getCityInfo = async (destinationCity) => {
-    console.log(GEONAMES_API_KEY, "YOUR GEONAMES_API_KEY");
-    // http://api.geonames.org/searchJSON?q={goingTOLocation}&username={GEONAMES_API_KEY}
-    const CityInfo_URL = `${GEONAMES_URL}?q=${destinationCity}&username=${GEONAMES_API_KEY}`
-    const response = await fetch(CityInfo_URL);
-    try {
-        const cityData = await response.json();
-        return cityData;
-    } catch (error) {
-        console.log(error)
-    }
-}
+  // http://api.geonames.org/searchJSON?q={goingTOLocation}&username={GEONAMES_API_KEY}
+  const cityInfoURL = `${GEONAMES_URL}?q=${destinationCity}&username=${GEONAMES_API_KEY}`;
+  const response = await fetch(cityInfoURL);
+  try {
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Function getWeatherInfo to get weather information from WEATHERBIT API ( city_name,country_code,high_temp,low_temp,description)
 const getWetherInfo = async (city_lattitude, city_longitude) => {
-    // https://api.weatherbit.io/v2.0/forecast/daily?city=London&key=e77f3a905d2a4f55ae18d2168074848e&lat=51.50853&lon=-0.12574
-    const weatherInfo_URL = `${WEATHERBIT_FORCAST_URL}?lat=${city_lattitude}&lon=${city_longitude}&key=${WEATHERBIT_API_KEY}`;
-    const response = await fetch(weatherInfo_URL);
-    console.log(response, "weatherData")
-
-    try {
-        const weatherData = await response.json();
-        return weatherData;
-    } catch (error) {
-        console.log(error)
-    }
+  // https://api.weatherbit.io/v2.0/forecast/daily?city=London&key=e77f3a905d2a4f55ae18d2168074848e&lat=51.50853&lon=-0.12574
+  const weatherInfoURL = `${WEATHERBIT_FORCAST_URL}?lat=${city_lattitude}&lon=${city_longitude}&key=${WEATHERBIT_API_KEY}`;
+  const response = await fetch(weatherInfoURL);
+  try {
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Function getCityImg to get  image of city from pixabay API
 const getCityImg = async (city) => {
-    // https://pixabay.com/api/?key={PIXALABAY_API_KEY}&q={contry_name}&image_type=photo
-    const cityImg_URL = `${PIXALABAY_URL}?key=${PIXALABAY_API_KEY}&q=${city}&image_type=photo`;
-    const response = await fetch(cityImg_URL);
-    try {
-        const cityImg = await response.json();
-        return cityImg;
-    } catch (error) {
-        console.log(error)
-    }
+  // https://pixabay.com/api/?key={PIXALABAY_API_KEY}&q={contry_name}&image_type=photo
+  const cityImgURL = `${PIXALABAY_URL}?key=${PIXALABAY_API_KEY}&q=${city}&image_type=photo`;
+  const response = await fetch(cityImgURL);
+  try {
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-// Export All Function 
-export { getCityInfo, getWetherInfo, getCityImg }
+// Export All Function
+export { getCityInfo, getWetherInfo, getCityImg };
