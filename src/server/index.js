0fb3a87bfaa291fca07a26be1c8cb71ed276157b
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-02-25 19:14:14 +0200

    fix server side

--- src/server/index.js
+++ src/server/index.js
@@ -1,56 +1,57 @@
-
+/* Dependencies */
 // Require Express to run server and routes
-const express = require('express')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mockAPIResponse = require("./mockAPI.js");
 
 // Start up an instance of app
-const app = express()
-
-/* Dependencies */
-const path = require('path')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-// const mockAPIResponse = require('./mockAPI.js')
-
+const app = express();
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // Cors for cross origin allowance
-app.use(cors())
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-let user_trip_data = {}
+let user_trip_data = {};
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve('src/client/views/index.html'))
-})
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // Post Route
-app.post('/trip', (req, res) => {
+app.post("/trip", (req, res) => {
+  const {
+    departingTime,
+    returningTime,
+    city_name,
+    country_code,
+    temp,
+    description,
+    duration,
+  } = req.body;
   const tripData = {
-    departingTime: req.body.departingTime,
-    returingTime: req.body.returingTime,
-    city_name: req.body.city_name,
-    country_code: req.body.country_code,
-    temp: req.body.temp,
-    description: req.body.description,
-    duration: req.body.duration
+    departingTime,
+    returningTime,
+    city_name,
+    country_code,
+    temp,
+    description,
+    duration,
   };
-  user_trip_data = (tripData)
-  res.send(user_trip_data)
-  console.log(user_trip_data, "user_trip_data")
-})
-
+  user_trip_data = tripData;
+  res.send(user_trip_data);
+});
 
 // Setup Server
-const port = 5000;
+const PORT = 5000;
 
 // designates what port the app will listen to for incoming requests
-app.listen(port, (error) => {
-  if (error) throw new Error(error)
-  console.log('Server listening on port ', `${port}`)
-})
+app.listen(PORT, (error) => {
+  if (error) throw new Error(error);
+  console.log("Server listening on port ", `${PORT}`);
+});
